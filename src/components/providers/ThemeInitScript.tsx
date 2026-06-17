19ff292be34f8b84b2da5e043fac829a681e53bb
Enes Filiz <enesfiliz7@hotmail.com>
2026-06-18 02:13:03 +0300

    Fix theme init script rendering

--- src/components/providers/ThemeInitScript.tsx
+++ src/components/providers/ThemeInitScript.tsx
@@ -1,8 +1,14 @@
+import Script from "next/script";
+
 import { PARSEL_THEME_INIT_SCRIPT } from "@/lib/parsel-theme";
 
 export function ThemeInitScript() {
   return (
-    <script
+    // Next App Router docs allow beforeInteractive scripts in the root layout.
+    // eslint-disable-next-line @next/next/no-before-interactive-script-outside-document
+    <Script
+      id="parsel-theme-init"
+      strategy="beforeInteractive"
       dangerouslySetInnerHTML={{ __html: PARSEL_THEME_INIT_SCRIPT }}
     />
   );
